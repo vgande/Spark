e1489a2f15ae951ccc1bb390de65fb4a5145355b
vgande <91749958+vgande@users.noreply.github.com>
2021-10-11 19:23:24 -0500

    Add files via upload

---
+++ WordCount.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.SparkContext
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+
+
+object WordCount extends App{
+  
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+  val sc = new SparkContext("local[*]","wordcount")
+  
+  val input = sc.textFile("C:\\Users\\venki\\Downloads\\Data\\search_data.txt")
+  
+  val words = input.flatMap(x => x.split(" "))
+  
+  val wordMap = words.map(x => (x,1))
+  
+  val finalCount = wordMap.reduceByKey((x,y) => (x+y))
+  
+  finalCount.collect.foreach(println)
+  
+}
